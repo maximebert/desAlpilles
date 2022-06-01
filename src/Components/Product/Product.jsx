51261d0225c078e0134eca1b40ba07840d7f6145
maximebert <maximebertinelli@gmail.com>
2022-06-01 13:54:05 +0200

    ajout products

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,87 +1,70 @@
-import {
-  FavoriteBorderOutlined,
-  SearchOutlined,
-} from "@material-ui/icons";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-
-const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.5s ease;
-  cursor: pointer;
-`;
+import { popularProducts } from "../../data";
+import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
-  flex: 1;
-  margin: 5px;
-  min-width: 280px;
-  height: 350px;
+  padding: 20px;
   display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5fbfd;
-  position: relative;
-  &:hover ${Info}{
-    opacity: 1;
-  }
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Circle = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-color: white;
-  position: absolute;
-`;
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-const Image = styled.img`
-  height: 75%;
-  z-index: 2;
-`;
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
 
-const Icon = styled.div`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
-  transition: all 0.5s ease;
-  &:hover {
-    background-color: #e9f5f5;
-    transform: scale(1.1);
-  }
-`;
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
-const Product = ({ item }) => {
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      <Circle />
-      <Image src={item.img} />
-      <Info>
-        <Icon>
-          <Link to={`/produit/${item.id}`}>
-          <SearchOutlined />
-          </Link>
-        </Icon>
-        <Icon>
-          <FavoriteBorderOutlined />
-        </Icon>
-      </Info>
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
 
-export default Product;
+export default Products;
--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -14,9 +14,14 @@ const cartSlice = createSlice({
             state.products.push(action.payload)
             state.total += action.payload.price * action.payload.quantity;
         },
+        removeProduct:(state, action) => {
+            state.quantity -=1;
+            state.products.push(action.payload)
+            state.total += action.payload.price * action.payload.quantity;
+        },
     },
 });
 
 
-export const {addProduct} = cartSlice.actions;
+export const {addProduct, removeProduct} = cartSlice.actions;
 export default cartSlice.reducer;
